4f75c509d77470edd3cc7654dd20756dc4b8cdce
DPX <1710829969@qq.com>
2025-03-02 03:00:44 +0800

    [dpx]删除hex生成

--- Tools/ardupilotwaf/chibios.py
+++ Tools/ardupilotwaf/chibios.py
@@ -414,16 +414,16 @@ def chibios_firmware(self):
     cleanup_task.set_run_after(generate_apj_task)
 
     bootloader_bin = self.bld.srcnode.make_node("Tools/bootloaders/%s_bl.bin" % self.env.BOARD)
-    if self.bld.env.HAVE_INTEL_HEX:
-        if os.path.exists(bootloader_bin.abspath()):
-            if int(self.bld.env.FLASH_RESERVE_START_KB) > 0:
-                hex_target = self.bld.bldnode.find_or_declare('bin/' + link_output.change_ext('_with_bl.hex').name)
-            else:
-                hex_target = self.bld.bldnode.find_or_declare('bin/' + link_output.change_ext('.hex').name)
-            hex_task = self.create_task('build_intel_hex', src=[bin_target[0], bootloader_bin], tgt=hex_target)
-            hex_task.set_run_after(cleanup_task)
-        else:
-            print("Not embedding bootloader; %s does not exist" % bootloader_bin)
+    # if self.bld.env.HAVE_INTEL_HEX:
+    #     if os.path.exists(bootloader_bin.abspath()):
+    #         if int(self.bld.env.FLASH_RESERVE_START_KB) > 0:
+    #             hex_target = self.bld.bldnode.find_or_declare('bin/' + link_output.change_ext('_with_bl.hex').name)
+    #         else:
+    #             hex_target = self.bld.bldnode.find_or_declare('bin/' + link_output.change_ext('.hex').name)
+    #         hex_task = self.create_task('build_intel_hex', src=[bin_target[0], bootloader_bin], tgt=hex_target)
+    #         hex_task.set_run_after(cleanup_task)
+    #     else:
+    #         print("Not embedding bootloader; %s does not exist" % bootloader_bin)
 
     if self.env.DEFAULT_PARAMETERS:
         default_params_task = self.create_task('set_default_parameters',
